9d4f86411b849b5ba241e3d31fdec3ebbae13022
Julia Bubnova <bubnova.j.i@gmail.com>
2022-04-03 17:56:59 +0200

    Add sockjs and stomp for websocket connection

--- src/components/playground/Playground.tsx
+++ src/components/playground/Playground.tsx
@@ -1,26 +1,68 @@
+import SockJS from "sockjs-client";
+import Stomp, { Client as StompClient } from "stompjs";
 import { getBackgroundImage } from "../../api/getImgUrl";
 import GameProvider from "../../game/gameContext/gameContext";
 import Enemy from "../enemy/Enemy";
 import Player from "../player/Player";
 import Table from "../table";
-import * as css from "./Playground.module.css";
+import css from "./Playground.module.css";
+
+let stompClient: StompClient | null = null;
 
 function Playground() {
+  function connectToWS() {
+    console.log("starting connection");
+    const endpoint = "hello";
+
+    const socket = new SockJS("http://192.168.2.104:8080/gs-guide-websocket");
+    stompClient = Stomp.over(socket);
+    stompClient.connect({}, (frame) => {
+      console.log("Connected: " + frame);
+      stompClient?.subscribe("/topic/test-response", (greeting) => {
+        console.log("received message: ");
+
+        console.log(JSON.parse(greeting.body));
+      });
+    });
+  }
+
+  function sendMsg() {
+    const message = "how are you";
+    stompClient?.send(
+      "/app/test-request",
+      {},
+      JSON.stringify({ content: message })
+    );
+  }
+
+  function closeConn() {
+    if (stompClient) {
+      stompClient.disconnect(() => {
+        console.log("successfully disconnected");
+      });
+    }
+  }
+
   return (
-    <div
-      className={css.layout}
-      style={{ backgroundImage: getBackgroundImage() }}
-    >
-      <GameProvider>
-        <div className={css.enemyPlayer}>
-          <Enemy />
-        </div>
-        <Table />
-        <div className={css.player}>
-          <Player />
-        </div>
-      </GameProvider>
-    </div>
+    <>
+      <button onClick={connectToWS}>connect</button>
+      <button onClick={sendMsg}>send msg</button>
+      <button onClick={closeConn}>close conn</button>
+      <div
+        className={css.layout}
+        style={{ backgroundImage: getBackgroundImage() }}
+      >
+        <GameProvider>
+          <div className={css.enemyPlayer}>
+            <Enemy />
+          </div>
+          <Table />
+          <div className={css.player}>
+            <Player />
+          </div>
+        </GameProvider>
+      </div>
+    </>
   );
 }
 
